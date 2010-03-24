9e1750a29b1da60d332a0ed36a7f8551d797ae27
yeago <yeago@tappedout.net>
2010-03-24 10:55:28 -0500

    attempting to fix a bug related to linking to custom comments

--- models.py
+++ models.py
@@ -3,10 +3,11 @@ import datetime
 from django.db import models
 
 from django.contrib import comments
+CommentModel = comments.get_model()
 
 class Karma(models.Model):
 	user = models.ForeignKey('auth.User')
-	comment = models.ForeignKey(comments.get_model())
+	comment = models.ForeignKey(CommentModel)
 	date_added = models.DateTimeField(default=datetime.datetime.now)
 	class Meta:
 		unique_together = ('user','comment',)
--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
 
-
 urlpatterns = patterns('comments_plus.views',
 	url('(?P<id>\d+)/karma-add/$','karma_add',name="karma_add"),
 	url('(?P<id>\d+)/karma-remove/$','karma_remove',name="karma_remove"),
