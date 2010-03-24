a288835405caa5444ed487e79a21e262d9112946
yeago <yeago@tappedout.net>
2010-03-23 20:02:31 -0500

    X

--- models.py
+++ models.py
@@ -4,11 +4,9 @@ from django.db import models
 
 from django.contrib import comments
 
-Comment = comments.get_model()
-
 class Karma(models.Model):
 	user = models.ForeignKey('auth.User')
-	comment = models.ForeignKey(Comment)
+	comment = models.ForeignKey(comments.get_model())
 	date_added = models.DateTimeField(default=datetime.datetime.now)
 	class Meta:
 		unique_together = ('user','comment',)
