7d65cd9b1d7782c09fdec750184528282d931fec
yeago <yeago@tappedout.net>
2010-04-08 01:48:58 -0500

    added templatetag which sorts by karma score.

---
+++ templatetags/comments_plus_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from django.db.models import Count
+from django.contrib.comments.templatetags.comments import BaseCommentNode
+
+register = template.Library()
+
+class KarmaCommentListNode(BaseCommentNode):
+    """Insert a list of comments into the context."""
+    def get_context_value_from_queryset(self, context, qs):
+        return list(qs.annotate(Count('karma')).order_by('-karma__count'))
+
+def get_karma_comment_list(parser, token):
+    return KarmaCommentListNode.handle_token(parser, token)
+
+register.tag(get_karma_comment_list)
