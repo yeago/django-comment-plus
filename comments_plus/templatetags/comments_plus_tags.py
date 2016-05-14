471672f45c4663c52ef6206338f1b8ba19b86724
=yeago <yeago999@gmail.com>
2016-05-14 17:57:57 +0000

    simplfiy, add reqs.txt

--- comments_plus/templatetags/comments_plus_tags.py
+++ comments_plus/templatetags/comments_plus_tags.py
@@ -1,8 +1,11 @@
 from django import template
 from django.template.loader import render_to_string
 from django.db.models import Count
-from django.db.models.query import QuerySet
-from django.contrib.comments.templatetags.comments import BaseCommentNode, CommentFormNode
+from django.contrib.contenttypes.models import ContentType
+from django_comments.templatetags.comments import BaseCommentNode
+import django_comments
+
+Comment = django_comments.get_model()
 
 register = template.Library()
 
@@ -45,62 +48,29 @@ def get_karma_comment_list(parser, token):
 register.tag(get_karma_comment_list)
 
 
-class RenderCommentStageNode(CommentFormNode):
-    """Render the comment strage directly"""
-    def __init__(self, qs=None, *args, **kwargs):
-        self.explicit_varname = qs
-        super(RenderCommentStageNode, self).__init__(*args, **kwargs)
+@register.simple_tag(takes_context=True)
+def render_comment_stage(context, instance, since=None, until=None, templates=None, comments=None):
+    ctype = ContentType.objects.get_for_model(instance)
+    templates = templates or [
+        "comments/%s/%s/stage.html" % (ctype.app_label, ctype.model),
+        "comments/%s/stage.html" % ctype.app_label,
+        "comments/stage.html"
+    ]
+    comment_list = comments or Comment.objects.for_model(instance)
+    if since:
+        comment_list = comment_list.filter(submit_date__gte=since)
+    if until:
+        comment_list = comment_list.filter(submit_date__lt=until)
 
-    @classmethod
-    def handle_token(cls, parser, token):
-        """Class method to parse render_comment_stage and return a Node."""
-        tokens = token.contents.split()
-        if tokens[1] != 'for':
-            raise template.TemplateSyntaxError("Second argument in %r tag must be 'for'" % tokens[0])
+    render_context = {
+        "request": context.get("request"),
+        "object": instance,
+        'comment_list': comment_list,
+    }
 
-        # {% render_comment_form for obj %}
-        if len(tokens) == 3 or (len(tokens) == 5 and tokens[3] == 'with'):
-            qs = None
-            if len(tokens) == 5:
-                qs = parser.compile_filter(tokens[4])
-            return cls(object_expr=parser.compile_filter(tokens[2]), qs=qs)
-
-        # {% render_comment_form for app.models pk %}
-        elif len(tokens) == 4:
-            return cls(
-                ctype=BaseCommentNode.lookup_content_type(tokens[2], tokens[0]),
-                object_pk_expr=parser.compile_filter(tokens[3])
-            )
-
-    def render(self, context, template_search_list=None):
-        ctype, object_pk = self.get_target_ctype_pk(context)
-        if object_pk:
-            defaults = [
-                "comments/%s/%s/stage.html" % (ctype.app_label, ctype.model),
-                "comments/%s/stage.html" % ctype.app_label,
-                "comments/stage.html"
-            ]
-            template_search_list = template_search_list or defaults
-            context.push()
-
-            render_context = {
-                "request": context.get("request"),
-                "object": self.object_expr.resolve(context),
-            }
-
-            if self.explicit_varname:
-                render_context['comment_list'] = self.explicit_varname.resolve(context)
-
-            stagestr = render_to_string(
-                template_search_list,
-                render_context,
-                context)
-            context.pop()
-            return stagestr
-        return ''
-
-
-def render_comment_stage(parser, token):
-    return RenderCommentStageNode.handle_token(parser, token)
-
-register.tag(render_comment_stage)
+    stagestr = render_to_string(
+        templates,
+        render_context,
+        context)
+    context.pop()
+    return stagestr
